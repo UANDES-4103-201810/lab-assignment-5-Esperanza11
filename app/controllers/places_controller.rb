cf29237689da9acb2e66c656a8fdc9343bed2105
Esperanza11 <eruribe@miuandes.cl>
2018-04-10 13:24:12 -0300

    goal 1

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,10 @@
+class PlacesController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  POST "/tickets", to: "tickets#create"
+
+  DELETE "/tickets", to: "tickets#destroy"
+
+  PATCH "/tickets", to: "tickets#update"
+
+  POST "/events", to: "events#create"
+
+  DELETE "/events", to: "events#destroy"
+
+  PATCH "/events", to: "events#update"
+
+  POST "/places", to: "places#create"
+
+  DELETE "/places", to: "places#destroy"
+
+  PATCH "/places", to: "places#update"
+
+  POST "/users", to: "users#create"
+
+  DELETE "/users", to: "users#destroy"
+
+  PATCH "/users", to: "users#update"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
